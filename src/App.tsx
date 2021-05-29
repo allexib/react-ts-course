c69399c276f474db8113330fca0089e6cc79f531
allexib <ooomegasib@gmail.com>
2021-05-29 20:49:20 +0400

    React.FunctionComponent, ReactFC, useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Card from "./components/Card";
+import Card, {CardVariant} from "./components/Card";
 
 const App = () => {
     return (
         <div>
-          <Card width='200px' height='200px'>
+          <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width='200px' height='200px'>
               <button>Кнопка</button>
               <div>text</div>
           </Card>
