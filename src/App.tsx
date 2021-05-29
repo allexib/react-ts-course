45c5a32e76db29360843ee854c290182d58a9454
allexib <ooomegasib@gmail.com>
2021-05-29 20:09:35 +0400

    first component, typysation props, interface cardprops

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Card from "./components/Card";
 
 const App = () => {
     return (
         <div>
-          Работает!
+          <Card width='200px' height='200px'>
+              <button>Кнопка</button>
+              <div>text</div>
+          </Card>
         </div>
     );
 };
