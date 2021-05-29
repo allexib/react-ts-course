d7e3d20c5cf744b6e42ac2a4c41c2cbb216992c7
allexib <ooomegasib@gmail.com>
2021-05-29 22:02:09 +0400

    typysation request axios,useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card, {CardVariant} from "./components/Card";
 import UserList from "./components/UserList";
 import {IUser} from "./types/types";
+import axios from "axios";
 
 const App = () => {
-    const users: IUser[] = [
-        {id: 1, name: 'Kama', email: 'faf@gag.cc', address: {city: 'Daga', street: 'Ara', zipcode: '111'}},
-        {id: 2, name: 'Gaba', email: 'faf@gag.cc', address: {city: 'Dag', street: 'Ara', zipcode: '111'}}
-    ]
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        fetchUsers()
+    },[]);
+    
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            setUsers(response.data)
+        } catch (e) {
+            alert(e)
+        }
+    }
+    
     return (
         <div>
             <Card variant={CardVariant.outlined} width='200px' height='200px'>
