37e50daae04c8d4ace0c38c78f1afaec51f3b696
allexib <ooomegasib@gmail.com>
2021-05-29 21:47:09 +0400

    component UserList, IUser, IAddress

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Card, {CardVariant} from "./components/Card";
+import UserList from "./components/UserList";
+import {IUser} from "./types/types";
 
 const App = () => {
+    const users: IUser[] = [
+        {id: 1, name: 'Kama', email: 'faf@gag.cc', address: {city: 'Daga', street: 'Ara', zipcode: '111'}},
+        {id: 2, name: 'Gaba', email: 'faf@gag.cc', address: {city: 'Dag', street: 'Ara', zipcode: '111'}}
+    ]
     return (
         <div>
-          <Card onClick={(num) => console.log('click', num)} variant={CardVariant.outlined} width='200px' height='200px'>
-              <button>Кнопка</button>
-              <div>text</div>
-          </Card>
+            <Card variant={CardVariant.outlined} width='200px' height='200px'>
+                <button>Кнопка</button>
+                <div>text</div>
+            </Card>
+            <UserList users={users}/>
         </div>
     );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,8 +9,7 @@ interface CardProps {
     width?: string;
     height?: string;
     variant: CardVariant;
-    onClick: (num: number) => void;
-
+    // onClick: (num: number) => void;
 }
 
 const Card: FC<CardProps> =
@@ -18,21 +17,14 @@ const Card: FC<CardProps> =
          width,
          height,
          variant,
-         onClick,
          children
      }) => {
-    const [state, setState] = useState(0);
+        const [state, setState] = useState(0);
         return (
-            <div style={{
-                width, height,
-                border: variant === CardVariant.outlined
-                    ? '1px solid gray'
-                    : 'none',
-                background: variant === CardVariant.primary
-                    ? 'lightgray'
-                    : ''
+            <div style={{width, height,
+                border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+                background: variant === CardVariant.primary ? 'lightgray' : ''
             }}
-            onClick={() => onClick(state)}
             >
                 {children}
             </div>
